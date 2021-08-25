6a2a7e1ff96976d0bb551626a39eec18c308a481
Alvaro Paez <paezguerraalvaro@gmail.com>
2021-08-26 00:41:46 +0200

    Added Texture2D extension method to copy a texture to ensure its data is readable (useful to interact with built-in icons).

--- Runtime/Extensions/Texture2DExtensions.cs
+++ Runtime/Extensions/Texture2DExtensions.cs
@@ -44,5 +44,43 @@
         /// <param name="rect">Rectangle in which to draw the texture.</param>
         /// <example><code>tintedIcon.Draw(triangleRect);</code></example>
         [PublicAPI] public static void Draw(this Texture2D texture, Rect rect) => GUI.DrawTexture(rect, texture);
+
+        /// <summary>Duplicates a texture2D. The copy is readable.</summary>
+        /// <param name="texture">The texture to copy.</param>
+        [PublicAPI] public static Texture2D Copy(this Texture2D texture)
+        {
+            // Create a temporary RenderTexture of the same size as the texture
+            RenderTexture tmp = RenderTexture.GetTemporary( 
+                                texture.width,
+                                texture.height,
+                                0,
+                                RenderTextureFormat.Default,
+                                RenderTextureReadWrite.Linear);
+
+            // Blit the pixels on texture to the RenderTexture
+            Graphics.Blit(texture, tmp);
+
+            // Backup the currently set RenderTexture
+            RenderTexture previous = RenderTexture.active;
+
+            // Set the current RenderTexture to the temporary one we created
+            RenderTexture.active = tmp;
+
+            // Create a new readable Texture2D to copy the pixels to it
+            Texture2D myTexture2D = new Texture2D(texture.width, texture.height);
+
+            // Copy the pixels from the RenderTexture to the new Texture
+            myTexture2D.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+            myTexture2D.Apply();
+
+            // Reset the active RenderTexture
+            RenderTexture.active = previous;
+
+            // Release the temporary RenderTexture
+            RenderTexture.ReleaseTemporary(tmp);
+
+            // "myTexture2D" now has the same pixels from "texture" and it's readable
+            return myTexture2D;
+        }
     }
 }
